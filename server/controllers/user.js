9ca0e23918b0691239b830658d2c9f554484a9ee
sauravshete2003 <sauravshete72@gmail.com>
2024-12-25 20:11:31 +0530

    add jwt and bcrypt author security add

---
+++ server/controllers/user.js
@@ -0,0 +1,62 @@
+import User from "./../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const postSignup = async (req, res) => {
+  const { name, password, rePassword, email, phone, address } = req.body;
+  if (!name || !password || !rePassword || !email || !phone || !address) {
+    return res.status(400).json({ message: "Please fill all the fields" });
+  }
+  if (password !== rePassword) {
+    return res
+      .status(400)
+      .json({ message: "Password and Confirm Password should be same" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    name,
+    password: await bcrypt.hash(password, hashedPassword),
+    email,
+    phone,
+    address,
+  });
+  try {
+    const savedUser = await user.save();
+    res.status(201).json({
+      message: "User created successfully",
+      data: {
+        name: savedUser.name,
+        email: savedUser.email,
+        phone: savedUser.phone,
+        address: savedUser.address,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message, succeess: false });
+  }
+};
+
+const postLogin = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and Password are required" });
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "Invalid Email or Password" });
+  }
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    return res.status(400).json({ message: "Invalid Email or Password" });
+  }
+  const token = jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+  res.setHeader("Authorization" , `Bearer ${token}`);
+  res.json({ message: "Login Successfull", data: { token } });
+
+};
+export { postSignup, postLogin };
--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
+import jwt from 'jsonwebtoken'
+
+import { postLogin, postSignup } from "./controllers/user.js";
 
 const app = express();
 app.use(cors());
@@ -21,6 +24,23 @@ const MONGO_URL = process.env.MONGO_URL;
 app.get("/health", (req, res) => {
   res.json("Server is running");
 });
+
+app.post('/signup' , postSignup);
+app.post('/login' , postLogin);
+app.get('/test', (req , res)=>{
+  const token = req.headers.authorization;
+  if(!token){
+    res.json({message : "No token found" , status : 401})
+  }
+  const tokenValue = token.split(" ")[1];
+  const decoded = jwt.verify(tokenValue , process.env.SECRET_KEY);
+  if (!decoded){
+    res.json({message : "Invalid token" , status : 401})
+  }
+  res.json({message : "Token is valid" , status : 200 , decoded : decoded
+    })
+})
+
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}✅`);
