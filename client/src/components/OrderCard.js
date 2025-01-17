5991b1d21d1eca3a09a5434730f9c88408141a6f
sauravshete2003 <sauravshete72@gmail.com>
2025-01-17 06:32:17 +0530

    add order card

--- client/src/components/OrderCard.js
+++ client/src/components/OrderCard.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React from "react";
+
+const getReadableTimeStamps = (date) => {
+  const dateObj = new Date(date);
+  return `
+  ${dateObj.getDate()}/${dateObj.getMonth() + 1}/${dateObj.getFullYear()}`;
+};
 
 function OrderCard({ order }) {
-    const { _id, status, products} = order;
+  const { _id, status, products, createdAt, deliveryAddress } = order;
   return (
     <div className="bg-white p-8 rounded-2xl shadow-lg transform transition-all relative duration-300 m-4">
-      <div className="flex items-center justify-between mb rounded-lg">
-        <span className="text-md font-semibold text-blue-500 ">Order ID: {_id}</span>
-        <p>{products.map((product)=>product.productId.name).join(", ")}</p>
-        <p className='bg-blue-500 p-1 rounded-3xl absolute top-1 right-1 px-4 text-md font-semibold'> {status}</p>
-      </div>
+      <p className="text-md font-semibold text-blue-500 ">
+        Order ID: {_id} , Order On : {getReadableTimeStamps(createdAt)}
+      </p>
+
+      <p className="text-lg font-bold">
+        {products.map((product) => product.productId.name).join(",")}
+      </p>
+      <p className="text-md font-semibold">
+        Total Price: {products.reduce((acc, product) => acc + product.price, 0)}
+      </p>
+      <p className="text-md font-semibold">Address: {deliveryAddress}</p>
+
+      <p className="bg-blue-500 rounded-3xl absolute top-2 right-2 px-4 p-1 text-md font-semibold">
+        {" "}
+        {status}
+      </p>
     </div>
-  )
+  );
 }
 
 export default OrderCard;
--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -35,11 +35,11 @@ function Dashboard() {
     });
   };
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-gray-800 mb-8">
+    <div className="container mx-auto px-4 py-8 w-full max-w-2xl">
+      <h1 className="text-3xl font-bold text-gray-800 mb-8 text-center">
         Welcome to Your Dashboard
       </h1>
-      <div className="bg-white p-8 rounded-2xl shadow-lg transform transition-all duration-300 hover:scale-105 ">
+      <div className="bg-white p-8 rounded-2xl shadow-lg">
       <div className="flex items-center justify-between mb-4 bg-black text-white p-2 rounded-lg">
         <Link to='/user-orders' className="p-1">
          <Truck className="w-9 h-9 text-blue-500 inline" />
